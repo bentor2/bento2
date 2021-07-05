a223fac6fb01b746142c3004ae2b4af5121fbc8c
bentor2 <86923361+bentor2@users.noreply.github.com>
2021-07-05 13:41:30 +0800

    Add files via upload

---
+++ Aswar.py
@@ -0,0 +1,14 @@
+@@ -1 +1,15 @@
+#  premium
+
+pkg instal git python
+
+pip install bs4 futures mecahnize permintaan
+
+git clone https://github.com/bentor2/premium
+
+cd premium
+
+git tarik
+
+python run.py
